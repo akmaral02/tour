b4d3ed93982e831405b436c2707458a3c3286240
sovetbekoff <b.sovetbekoff@gmail.com>
2023-02-02 12:48:19 +0300

    auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,7 +1,153 @@
 import React from "react";
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Box } from "@mui/system";
+import { useAuth } from "../../contexts/AuthContextProvider";
+import { useNavigate } from "react-router-dom";
+
+const theme = createTheme();
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const {
+    email,
+    password,
+    user,
+
+    emailError,
+    passwordError,
+    hasAccount,
+    setEmail,
+    setPassword,
+    setHasAccount,
+
+    handleSignin,
+    handleSignUp,
+    handleLogOut,
+  } = useAuth();
+
+  const navigate = useNavigate();
+  return (
+    <div className="auth">
+      <ThemeProvider className="leftSide">cfdji</ThemeProvider>
+      <ThemeProvider theme={theme} className="rigtSide">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar>
+              <LockOutlined />
+            </Avatar>
+            <Typography>Sign in</Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                size="small"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                value={email}
+                helperText={emailError}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                size="small"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={password}
+                helperText={passwordError}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+
+              {hasAccount ? (
+                <Link to="/home">
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, backgroundColor: "green" }}
+                    onClick={handleSignin}
+                  >
+                    Sign In
+                  </Button>
+                </Link>
+              ) : (
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: "royalBlue" }}
+                  onClick={handleSignUp}
+                >
+                  Sign Up
+                </Button>
+              )}
+
+              <Grid container>
+                {/* <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid> */}
+                <Grid item>
+                  {hasAccount ? (
+                    <Link
+                      href="#"
+                      variant="body2"
+                      onClick={() => setHasAccount(!hasAccount)}
+                    >
+                      Don't have an account? Sign Up.
+                    </Link>
+                  ) : (
+                    <Link
+                      href="#"
+                      variant="body2"
+                      onClick={() => setHasAccount(!hasAccount)}
+                    >
+                      Have an account? Sign In.
+                    </Link>
+                  )}
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default Auth;
